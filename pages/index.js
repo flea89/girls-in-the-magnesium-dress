fdd4d0389e5de8d42a4e97c0839957f7f26dcc2b
Paolo <paolo@potatolondon.com>
2022-03-20 11:27:08 +0000

    Move getCopy to root

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
-import { getPageCopy } from './utils/getPageCopy'
+import { getPageCopy } from '../utils/getPageCopy'
 
 export default function Ruutsu(context) {
 
