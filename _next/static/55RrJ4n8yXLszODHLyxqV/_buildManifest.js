a607c440d5573f6028f0d5a01c94ddb2f8784186
flea89 <flea89@users.noreply.github.com>
2022-03-20 10:02:19 +0000

    deploy: f85174a4ecf8b97870cd7f18224ca429c9199333

---
+++ _next/static/55RrJ4n8yXLszODHLyxqV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/3fa4bdfbf23075df.css","static/chunks/pages/index-68beb853542a8953.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
