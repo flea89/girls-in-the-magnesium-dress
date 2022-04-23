d9d6a417e3f1050d3101ad38f9d0f78633ebfaca
Paolo <paolo@potatolondon.com>
2022-04-23 23:46:07 +0100

    Add analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -10,6 +11,21 @@ export default function Document() {
         <meta property="og:site_name" content="The girls in the magnesium dress" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@400;700&display=swap" rel="stylesheet" />
+
+        <Script 
+          src="https://www.googletagmanager.com/gtag/js?id=G-2N0535XLV9"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-2N0535XLV9');
+          `}
+        </Script>
       </Head>
       <body>
         <Main />
