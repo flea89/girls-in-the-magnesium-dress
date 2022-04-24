5a544d8e99f445ec94ad65bb389ab4fc15f1abde
Paolo <paolo@potatolondon.com>
2022-04-24 14:10:39 +0100

    Move google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
+import Script from 'next/script'
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Script 
+      src="https://www.googletagmanager.com/gtag/js?id=G-2N0535XLV9"
+      strategy="afterInteractive"
+    />
+
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){window.dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-2N0535XLV9');
+      `}
+    </Script>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
